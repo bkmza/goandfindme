7df5fe2eb0c5d486662077339741582948c1f73e
bkmza <ilya.usikov@gmail.com>
2015-09-07 14:46:54 +0300

    Changed googlemaps key and icons.

--- DroidMapping/GoogleMapsKey.cs
+++ DroidMapping/GoogleMapsKey.cs
@@ -1,7 +1,7 @@
 ﻿using Android.App;
 
 #if RELEASE
-[assembly: MetaDataAttribute("com.google.android.maps.v2.API_KEY", Value="AIzaSyAhfSUNdDmxWJAexAiMflmcf7i_Zho409Y")]
+[assembly: MetaDataAttribute("com.google.android.maps.v2.API_KEY", Value="AIzaSyBMJeNKWT1XHGgp_QG67zR9Ze_NmM2wbpo")]
 #else
-[assembly: MetaDataAttribute("com.google.android.maps.v2.API_KEY", Value="AIzaSyAhfSUNdDmxWJAexAiMflmcf7i_Zho409Y")]
+[assembly: MetaDataAttribute("com.google.android.maps.v2.API_KEY", Value="AIzaSyBMJeNKWT1XHGgp_QG67zR9Ze_NmM2wbpo")]
 #endif
